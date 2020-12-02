ced36e6afd916bd520f417137bd57df3d67868ed
timvisee <tim@visee.me>
2020-12-02 15:39:29 +0100

    Use fast nom implementation in day02b

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -1,19 +1,38 @@
-use regex::Regex;
+#[macro_use]
+extern crate nom;
+
+use atoi::atoi;
+use nom::character::complete::alpha1;
 
 fn main() {
-    let re = Regex::new(r#"^(\d+)-(\d+) (.): (.+)$"#).unwrap();
     println!(
         "{}",
-        std::fs::read_to_string("./input.txt")
+        entries(&std::fs::read("./input.txt").unwrap())
             .unwrap()
-            .lines()
-            .map(|p| re.captures(p).unwrap())
-            .filter(|p| {
-                let (p1, p2): (usize, usize) = (p[1].parse().unwrap(), p[2].parse().unwrap());
-                let c = p[3].as_bytes()[0];
-                let s = p[4].as_bytes();
-                (s[p1 - 1] == c) != (s[p2 - 1] == c)
-            })
-            .count(),
+            .1
+            .into_iter()
+            .filter(|p| *p)
+            .count()
     );
 }
+
+named!(usize<&[u8], usize>, map_opt!(nom::character::complete::digit1, atoi));
+
+named!(minmax<&[u8], (usize, usize)>, separated_pair!(usize, char!('-'), usize));
+
+named!(needle<&[u8], u8>, terminated!(nom::number::complete::u8, char!(':')));
+
+named!(entry<&[u8], (usize, usize, u8, &[u8])>,
+    do_parse!(
+        mm: minmax >>
+        char!(' ') >>
+        needle: needle >>
+        char!(' ') >>
+        haystack: alpha1 >>
+        (mm.0, mm.1, needle, haystack)
+    )
+);
+
+named!(test_entry<&[u8], bool>, map!(entry, |e| ((e.3[e.0 - 1] == e.2) != (e.3[e.1 - 1] == e.2))));
+
+named!(entries<&[u8], Vec<bool>>, separated_list1!(tag!("\n"), test_entry));
