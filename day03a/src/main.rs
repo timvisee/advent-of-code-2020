98be91c0d2fa68b2692cc86ffcb8a0ba23e07fa6
timvisee <tim@visee.me>
2020-12-03 12:48:36 +0100

    Slightly improve day03

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -1,17 +1,12 @@
 fn main() {
-    let map: Vec<String> = std::fs::read_to_string("./input.txt")
-        .unwrap()
-        .lines()
-        .map(|l| l.into())
-        .collect();
-
-    let width = map[0].len();
+    let data = std::fs::read_to_string("./input.txt").unwrap();
+    let map: Vec<&str> = data.lines().collect();
 
     println!(
         "{}",
         (0..map.len() - 1)
             .map(|i| ((i + 1) * 3, i + 1))
-            .filter(|(x, y)| map[*y].chars().nth(*x % width).unwrap() == '#')
+            .filter(|(x, y)| map[*y].bytes().nth(*x % map[0].len()).unwrap() == b'#')
             .count()
     );
 }
--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -1,22 +1,15 @@
 fn main() {
-    let map: Vec<String> = std::fs::read_to_string("./input.txt")
-        .unwrap()
-        .lines()
-        .map(|l| l.into())
-        .collect();
-
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-
-    let width = map[0].len();
+    let data = std::fs::read_to_string("./input.txt").unwrap();
+    let map: Vec<&str> = data.lines().collect();
 
     println!(
         "{}",
-        slopes
-            .iter()
-            .map(|(xx, yy)| (0..map.len() - 1)
+        (0..5)
+            .map(|i| ((1 + i * 2) % 8, 1 + i / 4))
+            .map(|(xx, yy)| (0..)
                 .map(|i| ((i + 1) * xx, (i + 1) * yy))
-                .filter(|(_, y)| y < &map.len())
-                .filter(|(x, y)| map[*y].chars().nth(*x % width).unwrap() == '#')
+                .take_while(|(_, y)| y < &map.len())
+                .filter(|(x, y)| map[*y].bytes().nth(*x % map[0].len()).unwrap() == b'#')
                 .count())
             .product::<usize>()
     );
