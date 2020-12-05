e28e5a54b3cc9ba3e66e7449c29e52adf98c44e3
timvisee <tim@visee.me>
2020-12-05 18:24:19 +0100

    Don't format my oneliner

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -4,11 +4,7 @@ fn main() {
         std::fs::read("./input.txt")
             .unwrap()
             .chunks(11)
-            .map(|b| {
-                b[..10]
-                    .iter()
-                    .fold(0, |id, b| (id << 1) | (!b & 4) as usize >> 2)
-            })
+            .map(|b| b[..10].iter().fold(0, |id, b| (id << 1) | (!b & 4) as usize >> 2))
             .max()
             .unwrap(),
     );
