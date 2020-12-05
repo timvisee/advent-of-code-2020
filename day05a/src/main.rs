c03d28e411a1f7b308450545749345fc06072b27
timvisee <tim@visee.me>
2020-12-05 17:15:40 +0100

    Optimize day05

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -1,35 +1,15 @@
 fn main() {
     println!(
         "{}",
-        std::fs::read_to_string("./input.txt")
+        std::fs::read("./input.txt")
             .unwrap()
-            .lines()
-            .map(|l| row(l) * 8 + col(l))
+            .chunks(11)
+            .map(|b| b[..10].iter().fold(0, |mut id, b| {
+                id <<= 1;
+                id |= (!b & 0b100) as usize >> 2;
+                id
+            }))
             .max()
             .unwrap(),
     );
-}
-
-fn row(line: &str) -> usize {
-    let mut pos = 0;
-
-    for (i, a) in line.bytes().take(7).enumerate() {
-        if a == b'B' {
-            pos |= 1 << (7 - i - 1);
-        }
-    }
-
-    pos
-}
-
-fn col(line: &str) -> usize {
-    let mut pos = 0;
-
-    for (i, a) in line.bytes().skip(7).take(3).enumerate() {
-        if a == b'R' {
-            pos |= 1 << (3 - i - 1);
-        }
-    }
-
-    pos
 }
--- day05b/src/main.rs
+++ day05b/src/main.rs
@@ -1,37 +1,19 @@
 fn main() {
-    let mut ids = std::fs::read_to_string("./input.txt")
+    let mut ids = std::fs::read("./input.txt")
         .unwrap()
-        .lines()
-        .map(|l| row(l) * 8 + col(l))
+        .chunks(11)
+        .map(|b| {
+            b[..10].iter().fold(0, |mut id, b| {
+                id <<= 1;
+                id |= (!b & 0b100) as usize >> 2;
+                id
+            })
+        })
         .collect::<Vec<_>>();
     ids.sort_unstable();
 
     println!(
         "{}",
-        ids.windows(2).find(|num| num[0] == num[1] - 2).unwrap()[0] + 1,
+        ids.windows(2).find(|n| n[0] == n[1] - 2).unwrap()[0] + 1,
     );
-}
-
-fn row(line: &str) -> usize {
-    let mut pos = 0;
-
-    for (i, a) in line.bytes().take(7).enumerate() {
-        if a == b'B' {
-            pos |= 1 << (7 - i - 1);
-        }
-    }
-
-    pos
-}
-
-fn col(line: &str) -> usize {
-    let mut pos = 0;
-
-    for (i, a) in line.bytes().skip(7).take(3).enumerate() {
-        if a == b'R' {
-            pos |= 1 << (3 - i - 1);
-        }
-    }
-
-    pos
 }
