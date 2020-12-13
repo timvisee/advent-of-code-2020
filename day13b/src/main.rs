e363ecdaa215ffa6df6a5bcde074a1d71f7f0034
timvisee <tim@visee.me>
2020-12-13 19:17:45 +0100

    Clean up day13b

--- day13b/src/main.rs
+++ day13b/src/main.rs
@@ -1,17 +1,13 @@
 pub fn main() {
-    let (moduli, residues) = include_str!("../input.txt")
+    let lines: Vec<(i64, i64)> = include_str!("../input.txt")
         .lines()
         .nth(1)
         .unwrap()
         .split(',')
         .enumerate()
-        .filter_map(|(i, l)| l.parse::<i64>().ok().map(|l| (i, l)))
-        .fold((Vec::new(), Vec::new()), |(mut mo, mut re), (i, l)| {
-            mo.push(l);
-            re.push(l - i as i64);
-            (mo, re)
-        });
-    println!("{}", chinese_remainder(&residues, &moduli));
+        .filter_map(|(i, l)| l.parse().ok().map(|l| (l - i as i64, l)))
+        .collect();
+    println!("{}", crt(&lines));
 }
 
 #[inline(always)]
@@ -24,13 +20,12 @@ fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
     }
 }
 
-fn chinese_remainder(re: &[i64], mo: &[i64]) -> i64 {
-    let prod = mo.iter().product::<i64>();
-    re.iter()
-        .zip(mo)
-        .map(|(&re, &mo)| {
+fn crt(remo: &[(i64, i64)]) -> i64 {
+    let prod = remo.iter().map(|n| n.1).product::<i64>();
+    remo.iter()
+        .map(|(re, mo)| {
             let p = prod / mo;
-            re * ((egcd(p, mo).1 % mo + mo) % mo) * p
+            re * ((egcd(p, *mo).1 % mo + mo) % mo) * p
         })
         .sum::<i64>()
         % prod
