802ea595134113dfcb96c522ccb5c9d33c71402d
timvisee <tim@visee.me>
2021-01-17 22:14:51 +0100

    Improve day22b

--- day22b/src/main.rs
+++ day22b/src/main.rs
@@ -4,8 +4,8 @@ use std::collections::{HashSet, VecDeque};
 
 fn main() {
     let (p1, p2) = include_str!("../input.txt").split_once("\n\n").unwrap();
-    let mut p1: VecDeque<usize> = p1.lines().skip(1).map(|n| n.parse().unwrap()).collect();
-    let mut p2: VecDeque<usize> = p2.lines().skip(1).map(|n| n.parse().unwrap()).collect();
+    let mut p1 = p1.lines().skip(1).map(|n| n.parse().unwrap()).collect();
+    let mut p2 = p2.lines().skip(1).map(|n| n.parse().unwrap()).collect();
 
     println!(
         "{}",
@@ -26,18 +26,13 @@ fn play(p1: &mut VecDeque<usize>, p2: &mut VecDeque<usize>) -> bool {
         }
 
         let (c1, c2) = (p1.pop_front().unwrap(), p2.pop_front().unwrap());
-        if c1 <= p1.len() && c2 <= p2.len() {
-            if play(
-                &mut p1.make_contiguous()[..c1].to_owned().into(),
-                &mut p2.make_contiguous()[..c2].to_owned().into(),
-            ) {
-                p1.push_back(c1);
-                p1.push_back(c2);
-            } else {
-                p2.push_back(c2);
-                p2.push_back(c1);
-            }
-        } else if c1 > c2 {
+        if if c1 <= p1.len() && c2 <= p2.len() {
+            let mut sp1: VecDeque<_> = p1.make_contiguous()[..c1].to_owned().into();
+            let mut sp2: VecDeque<_> = p2.make_contiguous()[..c2].to_owned().into();
+            sp1.iter().max() >= sp2.iter().max() || play(&mut sp1, &mut sp2)
+        } else {
+            c1 > c2
+        } {
             p1.push_back(c1);
             p1.push_back(c2);
         } else {
