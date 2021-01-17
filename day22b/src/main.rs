b6ddf15593fe67589ada6a6c0255f22dff8f801b
timvisee <tim@visee.me>
2021-01-17 22:52:34 +0100

    Improve day22b

--- day22b/src/main.rs
+++ day22b/src/main.rs
@@ -1,15 +1,19 @@
 #![feature(str_split_once)]
 
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 
+const ALLOC_SIZE: usize = 3400;
+
+#[rustfmt::skip]
 fn main() {
     let (p1, p2) = include_str!("../input.txt").split_once("\n\n").unwrap();
-    let mut p1 = p1.lines().skip(1).map(|n| n.parse().unwrap()).collect();
-    let mut p2 = p2.lines().skip(1).map(|n| n.parse().unwrap()).collect();
+    let mut p1 = Deck::from(&p1.lines().skip(1).map(|n| n.parse().unwrap()).collect::<Vec<_>>());
+    let mut p2 = Deck::from(&p2.lines().skip(1).map(|n| n.parse().unwrap()).collect::<Vec<_>>());
 
     println!(
         "{}",
         if play(&mut p1, &mut p2) { p1 } else { p2 }
+            .as_slice()
             .iter()
             .rev()
             .enumerate()
@@ -18,28 +22,60 @@ fn main() {
     );
 }
 
-fn play(p1: &mut VecDeque<usize>, p2: &mut VecDeque<usize>) -> bool {
+fn play(p1: &Deck, p2: &Deck) -> bool {
     let mut turns = HashSet::new();
-    while turns.insert((p1.clone(), p2.clone())) {
+    while turns.insert((p1.as_slice(), p2.as_slice())) {
         if p1.len() == 0 || p2.len() == 0 {
-            return !p1.is_empty();
+            return p1.len() > 0;
         }
 
-        let (c1, c2) = (p1.pop_front().unwrap(), p2.pop_front().unwrap());
+        let (c1, c2) = (p1.pop(), p2.pop());
         if if c1 <= p1.len() && c2 <= p2.len() {
-            let mut sp1: VecDeque<_> = p1.make_contiguous()[..c1].to_owned().into();
-            let mut sp2: VecDeque<_> = p2.make_contiguous()[..c2].to_owned().into();
-            sp1.iter().max() >= sp2.iter().max() || play(&mut sp1, &mut sp2)
+            let mut sp1: Deck = Deck::from(&p1.as_slice()[..c1]);
+            let mut sp2: Deck = Deck::from(&p2.as_slice()[..c2]);
+            sp1.as_slice().iter().max() >= sp2.as_slice().iter().max() || play(&mut sp1, &mut sp2)
         } else {
             c1 > c2
         } {
-            p1.push_back(c1);
-            p1.push_back(c2);
+            p1.push(c1);
+            p1.push(c2);
         } else {
-            p2.push_back(c2);
-            p2.push_back(c1);
+            p2.push(c2);
+            p2.push(c1);
         }
     }
 
     true
+}
+
+struct Deck(usize, usize, [usize; ALLOC_SIZE]);
+
+impl Deck {
+    fn from(slice: &[usize]) -> Self {
+        let mut cards = [0; ALLOC_SIZE];
+        cards[..slice.len()].copy_from_slice(&slice);
+        Deck(0, slice.len(), cards)
+    }
+
+    fn as_slice(&self) -> &[usize] {
+        &self.2[self.0..self.1]
+    }
+
+    fn len(&self) -> usize {
+        self.1 - self.0
+    }
+
+    fn push(&self, card: usize) {
+        unsafe {
+            (*(&self.2 as *const _ as *mut [_; ALLOC_SIZE]))[self.1] = card;
+            *(&self.1 as *const _ as *mut usize) += 1;
+        }
+    }
+
+    fn pop(&self) -> usize {
+        unsafe {
+            *(&self.0 as *const _ as *mut usize) += 1;
+        }
+        self.2[self.0 - 1]
+    }
 }
