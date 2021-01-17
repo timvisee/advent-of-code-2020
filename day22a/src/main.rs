3bbaace746f6d7bcd489e0001fcf500fd6e45dc9
timvisee <tim@visee.me>
2021-01-17 20:23:34 +0100

    Fix day22a

--- day22a/src/main.rs
+++ day22a/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let mut p1: VecDeque<usize> = p1.lines().skip(1).map(|n| n.parse().unwrap()).collect();
     let mut p2: VecDeque<usize> = p2.lines().skip(1).map(|n| n.parse().unwrap()).collect();
 
-    while let Some((c1, c2)) = p1.pop_front().zip(p2.pop_front()) {
+    while !p2.is_empty() && !p2.is_empty() {
+        let (c1, c2) = p1.pop_front().zip(p2.pop_front()).unwrap();
         if c1 > c2 {
             p1.push_back(c1);
             p1.push_back(c2);
