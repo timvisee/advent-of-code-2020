fafac8f5e3ea6e352f3fb68fca38c78d24584454
timvisee <tim@visee.me>
2021-01-18 17:11:27 +0100

    Add day23a

--- README.md
+++ README.md
@@ -35,6 +35,7 @@ Timings include binary loading, execution, input and output timings.
 | [day 20](https://adventofcode.com/2020/day/20) | [`0.2ms`](./day20a/src/main.rs) | [`0.4ms`](./day20b/src/main.rs)  |
 | [day 21](https://adventofcode.com/2020/day/21) | [`0.7ms`](./day21a/src/main.rs) | [`0.6ms`](./day21b/src/main.rs)  |
 | [day 22](https://adventofcode.com/2020/day/22) | [`0.3ms`](./day22a/src/main.rs) | [`4.1ms`](./day22b/src/main.rs)  |
+| [day 23](https://adventofcode.com/2020/day/23) | [`0.3ms`](./day23a/src/main.rs) |                                  |
 
 |              | one-by-one                             | parallel                                   |
 |:-------------|:---------------------------------------|:-------------------------------------------|
---
+++ day23a/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut cups: Vec<u8> = include_bytes!("../input.txt")
+        .into_iter()
+        .filter(|&b| b != &b'\n')
+        .map(|b| b - b'0')
+        .collect();
+    let highest = *cups.iter().max().unwrap();
+
+    for _ in 0..100 {
+        let cur = cups[0];
+        let removed = [cups.remove(1), cups.remove(1), cups.remove(1)];
+        for target in (0..cur).rev().chain((cur..=highest).rev()) {
+            if let Some(pos) = cups.iter().position(|&c| c == target) {
+                for removed in removed.iter().rev() {
+                    cups.insert(pos + 1, *removed);
+                }
+                break;
+            }
+        }
+
+        cups.rotate_left(1);
+    }
+
+    let offset = cups.iter().position(|&c| c == 1).unwrap();
+    cups.rotate_left(offset);
+    println!(
+        "{}",
+        cups.iter()
+            .skip(1)
+            .map(|c| (c + b'0') as char)
+            .collect::<String>()
+    );
+}
